eb94da255fa77c5d40487ef77ba683d3e8c2c674
vandersonmgpb <vanderson1022@gmail.com>
2023-02-12 16:23:39 -0300

    Trabalhando e praticando com JavaScript

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 3
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 3)
+
+console.log('============================================')
+
+/* 
+var c = 1
+while (c<8){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+console.log('============================================')
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,13 @@
+/*var c = 1
+while (c<=5){
+    console.log(c)
+    c++
+}
+
+//*************************************** */
+
+console.log('Vai começar...')
+for(var c=0; c<=8; c++){
+    console.log(c)
+}
+console.log('Fim!')
